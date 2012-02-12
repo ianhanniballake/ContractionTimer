6b527c56d42c332e2489c5e1c987254fe287f18f
Ian Lake <ian.hannibal.lake@gmail.com>
2012-02-11 21:36:01 -0800

    Fixes dialog theme for pre-Honeycomb devices.
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/ianhanniballake/contractiontimer/ui/AboutDialogFragment.java
+++ src/com/ianhanniballake/contractiontimer/ui/AboutDialogFragment.java
@@ -2,12 +2,15 @@ package com.ianhanniballake.contractiontimer.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -31,9 +34,21 @@ public class AboutDialogFragment extends DialogFragment
 	@Override
 	public Dialog onCreateDialog(final Bundle savedInstanceState)
 	{
-		final AlertDialog.Builder builder = new AlertDialog.Builder(
-				getActivity());
-		final LayoutInflater inflater = getActivity().getLayoutInflater();
+		AlertDialog.Builder builder;
+		LayoutInflater inflater;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+		{
+			builder = new AlertDialog.Builder(getActivity());
+			inflater = getActivity().getLayoutInflater();
+		}
+		else
+		{
+			final ContextThemeWrapper wrapper = new ContextThemeWrapper(
+					getActivity(), R.style.DialogTheme);
+			builder = new AlertDialog.Builder(getActivity());
+			inflater = (LayoutInflater) wrapper
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
 		final View layout = inflater.inflate(R.layout.dialog_about, null);
 		builder.setTitle(R.string.app_name)
 				.setIcon(R.drawable.icon)
