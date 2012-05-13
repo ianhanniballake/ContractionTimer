6b3af7da7fffab8ff3dcd821fdabf326c5c2c4e1
Ian Lake <ian.hannibal.lake@gmail.com>
2012-05-12 23:33:52 -0700

    Fixes GH-73 - Ensures that PagerTitleStrip does not crash in onDetachedFromWindow
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

---
+++ src/com/ianhanniballake/contractiontimer/ui/SafePagerTitleStrip.java
@@ -0,0 +1,57 @@
+package com.ianhanniballake.contractiontimer.ui;
+
+import org.acra.ErrorReporter;
+
+import android.content.Context;
+import android.support.v4.view.PagerTitleStrip;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.ianhanniballake.contractiontimer.BuildConfig;
+
+/**
+ * PagerTitleStrip that ensures that onDetachedFromWindow does not propagate
+ * NullPointerExceptions
+ */
+public class SafePagerTitleStrip extends PagerTitleStrip
+{
+	/**
+	 * Default constructor
+	 * 
+	 * @param context
+	 *            Context
+	 */
+	public SafePagerTitleStrip(final Context context)
+	{
+		super(context);
+	}
+
+	/**
+	 * Default constructor
+	 * 
+	 * @param context
+	 *            Context
+	 * @param attrs
+	 *            XML Attributes
+	 */
+	public SafePagerTitleStrip(final Context context, final AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDetachedFromWindow()
+	{
+		try
+		{
+			super.onDetachedFromWindow();
+		} catch (final NullPointerException e)
+		{
+			if (BuildConfig.DEBUG)
+				Log.e(getClass().getSimpleName(),
+						"NullPointerException in onDetachedFromWindow", e);
+			else
+				ErrorReporter.getInstance().handleSilentException(e);
+		}
+	}
+}
