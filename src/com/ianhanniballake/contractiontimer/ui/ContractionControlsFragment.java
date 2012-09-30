fbf7b13a14889310db2e35b33b61e3efc750533f
Ian Lake <ian.hannibal.lake@gmail.com>
2012-09-30 12:49:54 -0700

    Fixes GH-102 - Validate Cursor in ContractionControlsFragment
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/ianhanniballake/contractiontimer/ui/ContractionControlsFragment.java
+++ src/com/ianhanniballake/contractiontimer/ui/ContractionControlsFragment.java
@@ -160,7 +160,8 @@ public class ContractionControlsFragment extends Fragment implements
 		final ToggleButton toggleContraction = (ToggleButton) view
 				.findViewById(R.id.toggleContraction);
 		toggleContraction.setEnabled(true);
-		final boolean contractionOngoing = data.moveToFirst()
+		final boolean contractionOngoing = data != null
+				&& data.moveToFirst()
 				&& data.isNull(data
 						.getColumnIndex(ContractionContract.Contractions.COLUMN_NAME_END_TIME));
 		toggleContraction.setChecked(contractionOngoing);
