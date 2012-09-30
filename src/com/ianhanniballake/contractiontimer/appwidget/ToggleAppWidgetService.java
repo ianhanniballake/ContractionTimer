dc969aa005adee951d8250c69e1bc49867ed1251
Ian Lake <ian.hannibal.lake@gmail.com>
2012-09-30 12:46:26 -0700

    Fixes GH-100 - Validate Cursor in ToggleAppWidgetService
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/ianhanniballake/contractiontimer/appwidget/ToggleAppWidgetService.java
+++ src/com/ianhanniballake/contractiontimer/appwidget/ToggleAppWidgetService.java
@@ -47,7 +47,8 @@ public class ToggleAppWidgetService extends IntentService
 		final Cursor data = getContentResolver().query(
 				ContractionContract.Contractions.CONTENT_URI, projection, null,
 				null, null);
-		final boolean contractionOngoing = data.moveToFirst()
+		final boolean contractionOngoing = data != null
+				&& data.moveToFirst()
 				&& data.isNull(data
 						.getColumnIndex(ContractionContract.Contractions.COLUMN_NAME_END_TIME));
 		RemoteViews views;
