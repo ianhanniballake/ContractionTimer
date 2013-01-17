dbb6e6052b66f38984bbe1b34002d3d2bfa52bb6
Ian Lake <ian.hannibal.lake@gmail.com>
2013-01-16 21:30:08 -0800

    Fixes GH-112 - Null Check View Activity Content Uri
    Ensure that the content uri we are attempting to parse for the ID is not
    null.
    
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

--- src/com/ianhanniballake/contractiontimer/ui/ViewActivity.java
+++ src/com/ianhanniballake/contractiontimer/ui/ViewActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -149,14 +150,17 @@ public class ViewActivity extends ActionBarFragmentActivity implements LoaderMan
 	{
 		adapter.swapCursor(data);
 		pagerAdapter.notifyDataSetChanged();
-		// A null intent/data means we should cancel out as we are in an
-		// indeterminate state
-		if (getIntent() == null || getIntent().getData() == null)
+		// A null content uri means we should cancel out as we are in an indeterminate state
+		final Uri contentUri = getIntent() == null ? null : getIntent().getData();
+		if (contentUri == null)
+		{
 			finish();
+			return;
+		}
 		final long contractionId;
 		try
 		{
-			contractionId = ContentUris.parseId(getIntent().getData());
+			contractionId = ContentUris.parseId(contentUri);
 		} catch (final NumberFormatException e)
 		{
 			if (BuildConfig.DEBUG)
