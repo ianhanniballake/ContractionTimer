974affebb50155611c8b378a332592bcb2118f1f
Ian Lake <ian.hannibal.lake@gmail.com>
2014-04-25 23:31:41 -0700

    Adds empty state text and renderer base

---
+++ ContractionTimer/src/glass/java/com/ianhanniballake/contractiontimer/TimerRenderer.java
@@ -0,0 +1,67 @@
+package com.ianhanniballake.contractiontimer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.View;
+
+import com.google.android.glass.timeline.DirectRenderingCallback;
+
+/**
+ * SurfaceHolder.Callback used to draw the timer on the timeline {@link com.google.android.glass.timeline.LiveCard}.
+ */
+public class TimerRenderer implements DirectRenderingCallback {
+    private final View mView;
+    private SurfaceHolder mHolder;
+    private boolean mRenderingPaused;
+
+    public TimerRenderer(Context context) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        mView = inflater.inflate(R.layout.main, null);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Measure and layout the view with the canvas dimensions.
+        int measuredWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+        int measuredHeight = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
+        mView.measure(measuredWidth, measuredHeight);
+        mView.layout(0, 0, mView.getMeasuredWidth(), mView.getMeasuredHeight());
+        draw();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The creation of a new Surface implicitly resumes the rendering.
+        mRenderingPaused = false;
+        mHolder = holder;
+        draw();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mHolder = null;
+    }
+
+    @Override
+    public void renderingPaused(SurfaceHolder holder, boolean paused) {
+        mRenderingPaused = paused;
+        draw();
+    }
+
+    public void draw() {
+        if (!mRenderingPaused && mHolder != null) {
+            Canvas canvas;
+            try {
+                canvas = mHolder.lockCanvas();
+            } catch (Exception e) {
+                return;
+            }
+            if (canvas != null) {
+                mView.draw(canvas);
+                mHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+}
--- ContractionTimer/src/glass/java/com/ianhanniballake/contractiontimer/TimerService.java
+++ ContractionTimer/src/glass/java/com/ianhanniballake/contractiontimer/TimerService.java
@@ -9,7 +9,14 @@ import com.google.android.glass.timeline.LiveCard;
 
 public class TimerService extends Service {
     private static final String LIVE_CARD_TAG = "contraction_timer";
+    private TimerRenderer mTimerRenderer;
     private LiveCard mLiveCard;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mTimerRenderer = new TimerRenderer(this);
+    }
 
     @Override
     public IBinder onBind(final Intent intent) {
@@ -20,7 +27,7 @@ public class TimerService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (mLiveCard == null) {
             mLiveCard = new LiveCard(this, LIVE_CARD_TAG);
-            mLiveCard.setDirectRenderingEnabled(true);
+            mLiveCard.setDirectRenderingEnabled(true).getSurfaceHolder().addCallback(mTimerRenderer);
             Intent menuIntent = new Intent(this, MenuActivity.class);
             menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
