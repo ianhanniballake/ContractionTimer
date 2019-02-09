5b69525afa2313558999984f091234cd4ecc9875
Ian Lake <ian.hannibal.lake@gmail.com>
2019-02-09 00:12:43 -0800

    Convert LicenseActivity to Kotlin

---
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/LicenseActivity.kt
@@ -0,0 +1,41 @@
+package com.ianhanniballake.contractiontimer.ui
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.ianhanniballake.contractiontimer.BuildConfig
+import com.ianhanniballake.contractiontimer.R
+
+/**
+ * License screen for the application. Shows as a dialog on large devices
+ */
+class LicenseActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "LicenseActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val isLockPortrait = preferences.getBoolean(Preferences.LOCK_PORTRAIT_PREFERENCE_KEY,
+                resources.getBoolean(R.bool.pref_lock_portrait_default))
+        if (BuildConfig.DEBUG)
+            Log.d(TAG, "Lock Portrait: $isLockPortrait")
+        requestedOrientation = if (isLockPortrait)
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        else
+            ActivityInfo.SCREEN_ORIENTATION_SENSOR
+    }
+}
