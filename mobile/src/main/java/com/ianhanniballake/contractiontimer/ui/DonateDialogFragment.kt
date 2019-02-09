66d6cd23420c7e29eff8b35bab0d3f3d60e9b872
Ian Lake <ian.hannibal.lake@gmail.com>
2019-02-09 11:34:07 -0800

    Convert DonateDialogFragment to Kotlin

---
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/DonateDialogFragment.kt
@@ -0,0 +1,134 @@
+package com.ianhanniballake.contractiontimer.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.SkuDetails
+import com.android.billingclient.api.SkuDetailsParams
+import com.android.billingclient.api.SkuDetailsResponseListener
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.ianhanniballake.contractiontimer.BuildConfig
+import com.ianhanniballake.contractiontimer.R
+import java.util.ArrayList
+
+class DonateDialogFragment : DialogFragment(), BillingClientStateListener, PurchasesUpdatedListener {
+    private lateinit var adapter: ArrayAdapter<CharSequence>
+    private val billingClient: BillingClient by lazy {
+        BillingClient.newBuilder(context)
+                .setListener(this)
+                .build()
+    }
+    private var skuDetailsList: List<SkuDetails> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1)
+        billingClient.startConnection(this)
+    }
+
+    override fun onBillingSetupFinished(responseCode: Int) {
+        if (!isAdded) {
+            return
+        }
+        val allSkus = ArrayList<String>()
+        if (BuildConfig.DEBUG) {
+            allSkus.add("android.test.purchased")
+            allSkus.add("android.test.canceled")
+            allSkus.add("android.test.refunded")
+            allSkus.add("android.test.item_unavailable")
+        }
+        val skuArray = resources.getStringArray(R.array.donate_in_app_sku_array)
+        allSkus.addAll(skuArray)
+        billingClient.querySkuDetailsAsync(SkuDetailsParams.newBuilder()
+                .setSkusList(allSkus)
+                .setType(BillingClient.SkuType.INAPP)
+                .build(),
+                SkuDetailsResponseListener { _, skuDetailsList ->
+                    val context = context ?: return@SkuDetailsResponseListener
+                    if (skuDetailsList == null) {
+                        return@SkuDetailsResponseListener
+                    }
+                    skuDetailsList.sortWith(Comparator { details1, details2 ->
+                        (details1.priceAmountMicros - details2.priceAmountMicros).toInt()
+                    })
+                    this@DonateDialogFragment.skuDetailsList = skuDetailsList
+                    adapter.clear()
+                    for (details in skuDetailsList) {
+                        adapter.add(details.description + " (" + details.price + ")")
+                    }
+                    adapter.notifyDataSetChanged()
+                    val bundle = Bundle().apply {
+                        putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "donate")
+                    }
+                    FirebaseAnalytics.getInstance(context).logEvent(
+                            FirebaseAnalytics.Event.VIEW_ITEM_LIST, bundle)
+                })
+        val purchasesResult = billingClient.queryPurchases(
+                BillingClient.SkuType.INAPP)
+        if (purchasesResult.purchasesList != null) {
+            for (purchase in purchasesResult.purchasesList) {
+                billingClient.consumeAsync(purchase.purchaseToken) { _, _ -> }
+            }
+        }
+    }
+
+    override fun onPurchasesUpdated(responseCode: Int, purchases: List<Purchase>?) {
+        if (purchases == null) {
+            return
+        }
+        for (purchase in purchases) {
+            billingClient.consumeAsync(purchase.purchaseToken) { consumeResponseCode, _ ->
+                if (consumeResponseCode == BillingClient.BillingResponse.OK) {
+                    Toast.makeText(context, R.string.donate_thank_you, Toast
+                            .LENGTH_LONG).show()
+                }
+                dismiss()
+            }
+        }
+    }
+
+    override fun onBillingServiceDisconnected() {
+        billingClient.startConnection(this)
+    }
+
+    override fun onDestroy() {
+        billingClient.endConnection()
+        super.onDestroy()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alertDialog = AlertDialog.Builder(context)
+                .setTitle(R.string.donate_header)
+                .setAdapter(adapter, null)
+                .create()
+        alertDialog.listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val skuDetails = skuDetailsList[position]
+            val sku = skuDetails.sku
+            val bundle = Bundle().apply {
+                putString(FirebaseAnalytics.Param.ITEM_ID, sku)
+                putString(FirebaseAnalytics.Param.ITEM_NAME, skuDetails.title)
+                putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "donate")
+            }
+            FirebaseAnalytics.getInstance(context).logEvent(
+                    FirebaseAnalytics.Event.VIEW_ITEM, bundle)
+            val responseCode = billingClient.launchBillingFlow(activity,
+                    BillingFlowParams.newBuilder()
+                            .setSku(sku)
+                            .setType(BillingClient.SkuType.INAPP)
+                            .build())
+            if (responseCode != BillingClient.BillingResponse.OK) {
+                dismiss()
+            }
+        }
+        return alertDialog
+    }
+}
