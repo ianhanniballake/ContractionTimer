8370ebbfb44d8c820ddd7333d8606a1c59c4c496
Ian Lake <ian.hannibal.lake@gmail.com>
2019-02-09 16:01:27 -0800

    Convert NoteDialogFragment to Kotlin

---
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/NoteDialogFragment.kt
@@ -0,0 +1,79 @@
+package com.ianhanniballake.contractiontimer.ui
+
+import android.app.Dialog
+import android.content.AsyncQueryHandler
+import android.content.ContentUris
+import android.content.ContentValues
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.widget.EditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.ianhanniballake.contractiontimer.BuildConfig
+import com.ianhanniballake.contractiontimer.R
+import com.ianhanniballake.contractiontimer.appwidget.AppWidgetUpdateHandler
+import com.ianhanniballake.contractiontimer.notification.NotificationUpdateService
+import com.ianhanniballake.contractiontimer.provider.ContractionContract
+
+/**
+ * Reset Confirmation Dialog box
+ */
+class NoteDialogFragment : DialogFragment() {
+    companion object {
+        private const val TAG = "NoteDialogFragment"
+        /**
+         * Argument key for storing/retrieving the contraction id associated with this dialog
+         */
+        const val CONTRACTION_ID_ARGUMENT = "com.ianhanniballake.contractiontimer.ContractionId"
+        /**
+         * Argument key for storing/retrieving the existing note associated with this dialog
+         */
+        const val EXISTING_NOTE_ARGUMENT = "com.ianhanniballake.contractiontimer.ExistingNote"
+    }
+
+    override fun onCancel(dialog: DialogInterface?) {
+        if (BuildConfig.DEBUG)
+            Log.d(TAG, "Received cancellation event")
+        super.onCancel(dialog)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val contractionId = arguments.getLong(NoteDialogFragment.CONTRACTION_ID_ARGUMENT)
+        val existingNote = arguments.getString(NoteDialogFragment.EXISTING_NOTE_ARGUMENT)
+        val builder = AlertDialog.Builder(activity)
+        val inflater = activity.layoutInflater
+        val layout = inflater.inflate(R.layout.dialog_note, null)
+        val input = layout.findViewById<EditText>(R.id.dialog_note_input)
+        if (existingNote.isNullOrBlank())
+            builder.setTitle(R.string.note_dialog_title_add)
+        else
+            builder.setTitle(R.string.note_dialog_title_edit)
+        input.setText(existingNote)
+        val context = activity
+        val asyncQueryHandler = object : AsyncQueryHandler(context.contentResolver) {
+            override fun onUpdateComplete(token: Int, cookie: Any, result: Int) {
+                AppWidgetUpdateHandler.createInstance().updateAllWidgets(context)
+                NotificationUpdateService.updateNotification(context)
+            }
+        }
+        @Suppress("DEPRECATION")
+        return builder.setView(layout).setInverseBackgroundForced(true)
+                .setPositiveButton(R.string.note_dialog_save) { _, _ ->
+                    if (BuildConfig.DEBUG)
+                        Log.d(TAG, "Received positive event")
+                    val noteEvent = if (existingNote.isNullOrBlank()) "note_add_saved" else "note_edit_saved"
+                    FirebaseAnalytics.getInstance(getContext()).logEvent(noteEvent, null)
+                    val updateUri = ContentUris.withAppendedId(
+                            ContractionContract.Contractions.CONTENT_ID_URI_BASE, contractionId)
+                    val values = ContentValues().apply {
+                        put(ContractionContract.Contractions.COLUMN_NAME_NOTE, input.text.toString())
+                    }
+                    asyncQueryHandler.startUpdate(0, 0, updateUri, values, null, null)
+                }.setNegativeButton(R.string.note_dialog_cancel) { _, _ ->
+                    if (BuildConfig.DEBUG)
+                        Log.d(TAG, "Received negative event")
+                }.create()
+    }
+}
