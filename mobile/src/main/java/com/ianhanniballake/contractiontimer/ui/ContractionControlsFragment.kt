63841a6e0efbcb563e9fc619c8cbcbb7a34d64c0
Ian Lake <ilake@google.com>
2023-05-24 00:01:43 +0000

    Convert ContractionControlsFragment to Compose

--- mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/ContractionControlsFragment.kt
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/ContractionControlsFragment.kt
@@ -1,135 +1,146 @@
 package com.ianhanniballake.contractiontimer.ui
 
-import android.annotation.SuppressLint
-import android.content.ContentUris
-import android.content.ContentValues
-import android.content.Context
-import android.database.Cursor
+import android.app.Application
 import android.os.Bundle
-import android.provider.BaseColumns
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.cursoradapter.widget.CursorAdapter
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.ianhanniballake.contractiontimer.BuildConfig
 import com.ianhanniballake.contractiontimer.R
 import com.ianhanniballake.contractiontimer.appwidget.AppWidgetUpdateHandler
+import com.ianhanniballake.contractiontimer.database.Contraction
+import com.ianhanniballake.contractiontimer.database.ContractionDatabase
+import com.ianhanniballake.contractiontimer.extensions.content
 import com.ianhanniballake.contractiontimer.notification.NotificationUpdateReceiver
-import com.ianhanniballake.contractiontimer.provider.ContractionContract
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.util.Date
+
+class ContractionControlsViewModel(application: Application): AndroidViewModel(application) {
+    private val dao = ContractionDatabase.getInstance(application).contractionDao()
+
+    val latestContraction = dao.latestContraction()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), null)
+
+    suspend fun startContraction() {
+        dao.insert(Contraction())
+    }
+
+    suspend fun stopContraction(contraction: Contraction) {
+        dao.update(contraction.copy(endTime = Date()))
+    }
+}
+
+private const val TAG = "ContractionControls"
+
+@Composable
+fun ContractionControls() {
+    val viewModel = viewModel<ContractionControlsViewModel>()
+    val latestContraction by viewModel.latestContraction.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    val analytics by remember(context) {
+        derivedStateOf {
+            FirebaseAnalytics.getInstance(context)
+        }
+    }
+    val scope = rememberCoroutineScope()
+    var ongoingJob by remember { mutableStateOf<Job?>(null) }
+    ContractionControlsScreen(
+        latestContraction,
+        enabled = ongoingJob == null,
+        onStartContraction = {
+            ongoingJob = scope.launch {
+                try {
+                    if (BuildConfig.DEBUG)
+                        Log.d(TAG, "Starting contraction")
+                    analytics.logEvent("control_start", null)
+                    viewModel.startContraction()
+                    AppWidgetUpdateHandler.createInstance().updateAllWidgets(context)
+                    NotificationUpdateReceiver.updateNotification(context)
+                } finally {
+                    ongoingJob = null
+                }
+            }
+        },
+        onStopContraction = { contractionToStop ->
+            scope.launch {
+                try {
+                    if (BuildConfig.DEBUG)
+                        Log.d(TAG, "Stopping contraction")
+                    analytics.logEvent("control_stop", null)
+                    viewModel.stopContraction(contractionToStop)
+                    AppWidgetUpdateHandler.createInstance().updateAllWidgets(context)
+                    NotificationUpdateReceiver.updateNotification(context)
+                } finally {
+                    ongoingJob = null
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ContractionControlsScreen(
+    latestContraction: Contraction?,
+    enabled: Boolean,
+    onStartContraction: () -> Unit,
+    onStopContraction: (contractionToStop: Contraction) -> Unit
+) {
+    val contractionOngoing = latestContraction != null && latestContraction.endTime == null
+    val scope = rememberCoroutineScope()
+    FloatingActionButton(
+        onClick = {
+            if (enabled) {
+                if (contractionOngoing) {
+                    onStopContraction(latestContraction!!)
+                } else {
+                    onStartContraction()
+                }
+            }
+        },
+        modifier = Modifier.padding(bottom = 16.dp, end = 16.dp)
+    ) {
+        if (contractionOngoing) {
+            Icon(painterResource(R.drawable.ic_notif_action_stop), stringResource(R.string.appwidget_contraction_stop))
+        } else {
+            Icon(painterResource(R.drawable.ic_notif_action_start), stringResource(R.string.appwidget_contraction_start))
+        }
+    }
+}
 
 /**
  * Fragment which controls starting and stopping the contraction timer
  */
-class ContractionControlsFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
-    companion object {
-        private const val TAG = "ContractionControls"
-    }
-
-    /**
-     * Cursor Adapter which holds the latest contraction
-     */
-    private lateinit var adapter: CursorAdapter
-
-    private lateinit var fab: FloatingActionButton
-
-    private var contractionOngoing = false
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        adapter = object : CursorAdapter(activity, null, 0) {
-            override fun bindView(view: View, context: Context, cursor: Cursor) {
-                // Nothing to do
-            }
-
-            override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View? {
-                return null
-            }
-        }
-        loaderManager.initLoader(0, null, this)
-    }
-
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        val projection = arrayOf(
-            BaseColumns._ID,
-            ContractionContract.Contractions.COLUMN_NAME_START_TIME,
-            ContractionContract.Contractions.COLUMN_NAME_END_TIME
-        )
-        return CursorLoader(
-            requireContext(), requireActivity().intent!!.data!!, projection,
-            null, null, null
-        )
-    }
+class ContractionControlsFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_contraction_controls, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fab = view.findViewById(R.id.fab)
-        fab.setOnClickListener {
-            // Disable the button to ensure we give the database a chance to
-            // complete the insert/update
-            view.isEnabled = false
-            val analytics = FirebaseAnalytics.getInstance(requireContext())
-            if (!contractionOngoing) {
-                if (BuildConfig.DEBUG)
-                    Log.d(TAG, "Starting contraction")
-                analytics.logEvent("control_start", null)
-                // Start a new contraction
-                val context = requireContext()
-                GlobalScope.launch {
-                    context.contentResolver.insert(ContractionContract.Contractions.CONTENT_URI,
-                            ContentValues())
-                    AppWidgetUpdateHandler.createInstance().updateAllWidgets(context)
-                    NotificationUpdateReceiver.updateNotification(context)
-                }
-            } else {
-                if (BuildConfig.DEBUG)
-                    Log.d(TAG, "Stopping contraction")
-                analytics.logEvent("control_stop", null)
-                val newEndTime = ContentValues()
-                newEndTime.put(ContractionContract.Contractions.COLUMN_NAME_END_TIME, System.currentTimeMillis())
-                val latestContractionId = adapter.getItemId(0)
-                val updateUri = ContentUris.withAppendedId(
-                        ContractionContract.Contractions.CONTENT_ID_URI_BASE, latestContractionId)
-                // Add the new end time to the last contraction
-                val context = requireContext()
-                GlobalScope.launch {
-                    context.contentResolver.update(updateUri,
-                            newEndTime, null, null)
-                    AppWidgetUpdateHandler.createInstance().updateAllWidgets(context)
-                    NotificationUpdateReceiver.updateNotification(context)
-                }
-            }
-        }
-    }
-
-    override fun onLoaderReset(loader: Loader<Cursor>) {
-        adapter.swapCursor(null)
-    }
-
-    @SuppressLint("Range")
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        adapter.swapCursor(data)
-        fab.isEnabled = true
-        contractionOngoing = (data != null && data.moveToFirst()
-                && data.isNull(data.getColumnIndex(ContractionContract.Contractions.COLUMN_NAME_END_TIME)))
-        if (contractionOngoing) {
-            fab.setImageResource(R.drawable.ic_notif_action_stop)
-        } else {
-            fab.setImageResource(R.drawable.ic_notif_action_start)
-        }
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = content {
+        ContractionControls()
     }
 }
