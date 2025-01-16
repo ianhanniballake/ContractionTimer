a911af0ceb3079157944750fd04548e71f6f2901
Ian Lake <ilake@google.com>
2025-01-16 00:04:21 +0000

    Add first draft of Compose based MainActivity

---
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ianhanniballake.contractiontimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ianhanniballake.contractiontimer.ui.theme.ContractionTimerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ContractionTimerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
---
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.ianhanniballake.contractiontimer.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
