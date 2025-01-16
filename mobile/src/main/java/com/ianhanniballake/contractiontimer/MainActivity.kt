e45e88e96308ca87d7936e09c0cbaa1596ad7e1d
Ian Lake <ilake@google.com>
2025-01-16 00:19:32 +0000

    Embed ContractionList in MainActivity

--- mobile/src/main/java/com/ianhanniballake/contractiontimer/MainActivity.kt
+++ mobile/src/main/java/com/ianhanniballake/contractiontimer/MainActivity.kt
@@ -4,29 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.ianhanniballake.contractiontimer.ui.ContractionList
 import com.ianhanniballake.contractiontimer.ui.theme.ContractionTimerTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ContractionTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        LargeTopAppBar(
+                            title = {
+                                Text(stringResource(R.string.app_name))
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        ContractionList(showNoteDialog = { contraction ->
+                            TODO("Handle showing $contraction")
+                        })
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
 }
